8364c7e2f5bf622a47e87ad99088db1e90bf670b
BANDA-XD <mohamed.banda.xd@gmail.com>
2025-02-04 11:23:19 +0100

    Update BANDA.py

--- BANDA.py
+++ BANDA.py
@@ -1,9 +1,11 @@
-#!/data/data/com.termux/files/usr/bin/python
-import platform,os
-#####
-os.system("git pull")
+import os, sys, platform,time
+ 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import BANDA
+    os.system('clear')
+    os.system('git pull')
+    import BANDA   
 elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
     import BANDA
